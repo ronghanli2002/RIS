68dc96d18fff191235d3e1854e2e13657ff45a69
Ronghan Li <146203214+LiRonghan0509@users.noreply.github.com>
2024-10-31 13:04:01 -0500

    Create password.sh

---
+++ password.sh
@@ -0,0 +1,7 @@
+# password: f5apAFT4S60wSwCb
+cd /storage1/fs1/yeli/Active/l.ronghan
+
+pip install ipykernel
+python -m ipykernel install --user --name=transformer
+
+jupyter notebook --config config.py 
